46b99af3f0e4b278aff4f0a5ac68d4efc0e22522
Yash-1706 <gaikwad17yash@gmail.com>
2024-11-12 19:00:58 +0530

    Create YashGaikwad.js

---
+++ messages/YashGaikwad.js
@@ -0,0 +1,6 @@
+let messageData = {
+    author: "Yash Gaikwad",
+    message: "Happy Birthday! 🎉\n\nI just wanted to express my gratitude for being an incredible teacher. You make our classes so much more enjoyable, and to be honest, I genuinely look forward to attending!\n\nYour enthusiasm and the way you simplify concepts make even the challenging material seem achievable.\n\nWishing you a day filled with all the things you love most. Here’s to more laughter, coding adventures, and great memories in the future!"
+};
+
+data[messageData.author] = messageData;
