78d91fe5d7d40558c075a56bd0fc9d6306032c12
Prabal Patra <p2005p5499p@gmail.com>
2024-11-12 17:15:47 +0530

    Prabal's Wish

---
+++ messages/Prabal.js
@@ -0,0 +1,10 @@
+let messageData = {
+    author: "Prabal Patra",
+    message: "🎉 Happy Birthday, Mrinal Sir! 🎂 \
+                I hope your special day is as awesome as your lessons! 😄 \
+                Thank you for making Web Development so much fun. \
+                Your passion for teaching truly shines through, and it's been a privilege to learn from you. \
+                Wishing you a birthday filled with happiness, and may the year ahead bring you joy and fulfillment!"
+};
+
+data[messageData.author] = messageData;
