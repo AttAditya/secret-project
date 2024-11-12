182d05afc1fab205e9d2a3afe38faa3efb3d577d
Ashmit <166328268+BinaryBhakti@users.noreply.github.com>
2024-11-12 18:25:18 +0530

    Ashmit.js

---
+++ messages/Ashmit.js
@@ -0,0 +1,6 @@
+let messageData = {
+    author: "Ashmit Kumar Nayak",
+    message: "Wishing you a very happy birthday, sir! 🤩🥳🥳 It's always a pleasure learning from you❤️. Thank you for making our web dev journey as smooth as bug-free code🤩 and as fun as hitting 'run' without any errors popping up! \ Thank You so much sir❤️"
+};
+
+data[messageData.author] = messageData;
