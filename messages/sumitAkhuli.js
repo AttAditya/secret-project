b294acadbfa7a16f178dd6436183d932f9c5ca20
akhulisumit <sumitakhuli5022@gmail.com>
2024-11-12 19:59:13 +0530

    wish

---
+++ messages/sumitAkhuli.js
@@ -0,0 +1,7 @@
+let messageData = {
+    author: "Sumit Akhuli",
+    message: "Wishing you a Very Happy Birthday, Sir. Enjoyyyyy you Day! Also thank you for making this Web Dev journey so Special and simplified and helping to join this world of Open Source. The way you teach really motivates me to take a deep dive in these field of development"
+};
+
+data[messageData.author] = messageData;
+
